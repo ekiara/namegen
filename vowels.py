4315c4a66bbed101ddaa0a21352f0e5dcb44dd41
Eric Kiara <ekiara@gmail.com>
2012-04-03 00:03:44 +0000

    vowels

--- vowels.py
+++ vowels.py
@@ -8,22 +8,31 @@ from sys import argv
 
 VOWELS = "aeiou"
 
-name = "Eric"
+def get_vowel_index(name):
+    vowel_index = []
+    for i in range(len(name.lower())):
+        if VOWELS.__contains__(name.lower()[i]):
+            vowel_index.append(i)
+    return vowel_index
 
-vowel_index = []
+def get_name_list(vowel_index, name_list):
+    for jina in name_list:
+        for i in vowel_index:
+            for j in VOWELS:
+                temp_name = jina.lower()
+                temp_name = temp_name[:i] + j + temp_name[i+1:]
+                if name_list.__contains__(temp_name) == False:
+                    name_list.append(temp_name)
+    return name_list
 
-name_list = []
+def main(name):
+    vi = get_vowel_index(name.lower())
+    nl = [name.lower()]
+    print "vowel index is now: %r, and namelist is now: %r" % (vi,nl)
+    for i in vi:
+        nl = get_name_list(vi, nl)
+    print "Completed generating names."
+    print nl
+    print "Total number of names generated: %d" % len(nl)
 
-for i in range(len(name.lower())):
-    if VOWELS.__contains__(name.lower()[i]):
-        vowel_index.append(i)
-
-def mutate(name_list):
-    for i in vowel_index:
-        for j in VOWELS:
-            temp_name = name.lower()
-            if name_list.__contains__(temp_name) == False:
-                name_list.append(temp_name)
-    print name_list
-
-
+main(argv[1])
