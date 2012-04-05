1c1b66e1a6000f286d081bc7fe64bb71f1f6f7c9
Eric Kiara <ekiara@gmail.com>
2012-04-05 01:50:48 +0000

    Refactor. Eliminate nested loop.

--- vowels.py
+++ vowels.py
@@ -1,51 +1,51 @@
 #!/usr/bin/env python
 
 """
+Synopsis: prog name
+
+Given a name as a string generate every permutation of that name using a set
+of rules such as vowel substitution, "hard sound" substitution.
+
+Return a list of strings with all name permutations.
 
 """
 
-from sys import argv
 
-VOWELS = "aeiou"
+def permutate_name(name):
+    for night in range(len(name)):
+        print "night: ", night
 
-def get_vowel_index(name):
-    vowel_index = []
-    for i in range(len(name.lower())):
-        if VOWELS.__contains__(name.lower()[i]):
-            vowel_index.append(i)
-    return vowel_index
+        temp_vowel_index = vowel_index(name, night)
+        print "temp_vowel_index: ", temp_vowel_index
 
-def get_name_list(vowel_index, name_list):
-    for jina in name_list:
-        for i in vowel_index:
-            for j in VOWELS:
-                temp_name = jina.lower()
-                temp_name = temp_name[:i] + j + temp_name[i+1:]
-                if name_list.__contains__(temp_name) == False:
-                    name_list.append(temp_name)
-    return name_list
+        for i in "aeiou":
+            temp_name = ""
+            #print "INITIAL temp_name: ", temp_name
 
-def process_single_name(name):
-    temp_vowel_index = get_vowel_index(name.lower())
-    temp_name_list = [name.lower()]
-    for current_vowel in temp_vowel_index:
-        temp_name_list = get_name_list(temp_vowel_index, temp_name_list)
-    print "Completed \"%s\": Total number of names generated: %d" % (name, len(temp_name_list))
-    return temp_name_list
+            for j in range(len(name)):
+                if j == temp_vowel_index:
+                    temp_name += i
+                    #print "IF temp_name: ", temp_name
 
-def name_hopper(big_list):
-    full_list = []
-    for current_name in big_list:
-        full_list.append( process_single_name(current_name) )
-    unwrapped_list = []
-    for x in full_list:
-        print "Length of x:", len(x)
-        for y in x:
-            unwrapped_list.append(y)
-    print "Unwrapped list site: ", len(unwrapped_list)
-    f = open("unwrapped_list.txt", "w")
-    for z in unwrapped_list:
-        f.write(z + "\n")
-    f.close()
+                else:
+                    temp_name += name[j]
+                    #print "ELSE temp_name" , temp_name
 
-name_hopper( ["Abdi","Abdul","Abdullahi","Adega","Akinyemi","Amir","Aroni","Bahraini","Bijal","Boit","Boris","Detai","Eegan","Farah","Fatuma","Gacheru","Gandii","Mahindra","Joram","Kabul","Karri","Karua","Kawhaja","Kimani","Kipchogi","Kofi","Kwach","Machozi","Mbaabu","Muchiri","Mugambi","Nyandarua","Okeyo","Olomide","Osire","Rehal"] )
+        print "Mutated Name: ", temp_name
+
+
+def vowel_index(name, offset):
+    for i in "aeiou":
+        #print "Checking: ", i
+        try:
+            vowel_index = name.lower().index(i)
+        except ValueError:
+            continue
+        return vowel_index
+    return None
+
+
+def main():
+    permutate_name("Eric")
+
+main()
